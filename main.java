467e49c81add5453ff6d46e36767bd277dac9fae
meggie123 <meghanabura@gmail.com>
2022-06-02 15:01:25 +0530

    Welcome To Line Comparision Computation Program

---
+++ main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Day8LineComparision;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Welcome TO Line Comparison Computation Program");
+    }
+}
